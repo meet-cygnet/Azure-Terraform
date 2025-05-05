10b5fa1d04fa46d0622dd5b265e566bd162f6ba8
anjali-tandel <anjali.tandel@cygnet.one>
2025-05-05 09:08:46 +0000

    pushing aks module changes

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,62 +1,36 @@
-# resource "azurerm_role_assignment" "acr_pull" {
-#   scope                = var.acr_id
-#   role_definition_name = "AcrPull"
-#   principal_id         = var.client_id
-# }
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  dns_prefix          = var.cluster_name
+  dns_prefix         = var.dns_prefix
   kubernetes_version  = var.kubernetes_version
-  outbound_type       = var.outbound_type
 
+  private_cluster_enabled = true
+  # private_dns_zone_id    = var.private_dns_zone_id
+  
   default_node_pool {
     name                = var.default_node_pool_name
-    node_count          = var.system_node_count
-    vm_size             = var.system_node_vm_size
-    os_disk_size_gb     = var.node_pool_os_disk_size_gb
-    os_disk_type        = var.default_node_pool_os_disk_type
-    max_pods            = var.node_pool_max_pods
-    vnet_subnet_id      = var.system_subnet_id
+    node_count          = var.default_node_pool_node_count
+    vm_size             = var.default_node_pool_vm_size
+    os_disk_size_gb     = var.default_node_pool_os_disk_size_gb
+    vnet_subnet_id      = var.subnet_id
     enable_auto_scaling = var.enable_auto_scaling
-    min_count           = var.system_node_min_count
-    max_count           = var.system_node_max_count
-  }
-
-  identity {
-    type = "SystemAssigned"
-    # type         = "UserAssigned"
-    # identity_ids = [var.identity_id]
+    min_count           = var.min_count
+    max_count           = var.max_count
+    max_pods            = var.max_pods
   }
 
   network_profile {
-    network_plugin    = "azure"
-    network_policy    = "azure"
-    load_balancer_sku = var.load_balancer_sku
-    outbound_type     = var.outbound_type
+    network_plugin     = "azure"
+    outbound_type = var.outbound_type
+    # service_cidr       = var.service_cidr
+    # dns_service_ip     = var.dns_service_ip
   }
 
-  private_cluster_enabled = var.private_cluster_enabled
-  # private_dns_zone_id     = var.private_dns_zone_id
-
-  sku_tier = var.sku_tier
+  identity {
+    type         = "SystemAssigned"
+    # identity_ids = [var.user_assigned_identity_id]
+  }
 
   tags = var.tags
-}
-
-# resource "azurerm_kubernetes_cluster_node_pool" "user" {
-#   name                  = "user"
-#   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-#   vm_size               = var.user_node_vm_size
-#   node_count            = var.user_node_count
-#   vnet_subnet_id        = var.user_subnet_id
-#   min_count             = var.user_node_min_count
-#   max_count             = var.user_node_max_count
-#   os_disk_size_gb       = var.node_pool_os_disk_size_gb
-#   max_pods              = var.node_pool_max_pods
-
-#   tags = var.tags
-# }
-
+} 
--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,21 +1,48 @@
-output "aks_id" {
-  description = "ID of the AKS cluster"
+output "cluster_id" {
+  description = "The ID of the Kubernetes Cluster"
   value       = azurerm_kubernetes_cluster.aks.id
 }
 
-output "aks_fqdn" {
-  description = "FQDN of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks.fqdn
-}
-
-output "aks_private_fqdn" {
-  description = "Private FQDN of the AKS cluster"
-  value       = azurerm_kubernetes_cluster.aks.private_fqdn
+output "cluster_name" {
+  description = "The name of the Kubernetes Cluster"
+  value       = azurerm_kubernetes_cluster.aks.name
 }
 
 output "kube_config" {
-  description = "Kubernetes configuration"
+  description = "The Kubernetes configuration for the cluster"
   value       = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive   = true
-} 
+}
 
+output "host" {
+  description = "The Kubernetes cluster server host"
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].host
+}
+
+output "client_key" {
+  description = "Base64 encoded private key used by clients to authenticate to the Kubernetes cluster"
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+  sensitive   = true
+}
+
+output "client_certificate" {
+  description = "Base64 encoded public certificate used by clients to authenticate to the Kubernetes cluster"
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+  sensitive   = true
+}
+
+output "cluster_ca_certificate" {
+  description = "Base64 encoded public CA certificate used as the root of trust for the Kubernetes cluster"
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
+  sensitive   = true
+}
+
+output "node_resource_group" {
+  description = "The auto-generated Resource Group which contains the resources for this Managed Kubernetes Cluster"
+  value       = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+
+output "fqdn" {
+  description = "The FQDN of the Azure Kubernetes Managed Cluster"
+  value       = azurerm_kubernetes_cluster.aks.fqdn
+} 
